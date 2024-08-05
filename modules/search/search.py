6dd630f7714ddd4773aea9effbb45f2994b926a2
ahmad <ahmadalkhalawi@gmail.com>
2024-08-05 14:18:37 +0300

    hllo

--- modules/search/search.py
+++ modules/search/search.py
@@ -5,7 +5,7 @@ from ...interpreter.interpret import interpret
 
 latex_grammar = open("modules/lat2lean/latex_grammar.lark").read()
 lean_grammar = open("modules/lat2lean/lean_grammar.lark").read()
-searchl = ["simp", "aesop", "ring_nf", "exact?", "omega", "rw?","norm_num"]
+searchl = ["ring_nf", "aesop", "exact?"]
 
 my_parser = Lark(latex_grammar)
 my_parse = my_parser.parse
@@ -23,23 +23,7 @@ def toLean(text):
         elif l.strip()=="<Math>": 
             mode="Math"
         elif l.strip()=="<Auto>": 
-            s += " := by "
-            s2 = s
-            ner = interpret(s2+"done")[1]
-            found = False
-            for x in searchl:
-                s2 += x
-                s2 += "\n"
-                ner2 = interpret(s2)[1]
-                print([ner, ner2])
-                if ner2 < ner:
-                    s = s2
-                    found = True
-                    break
-                else: s2 = s
-            if found == False:
-                s+= "sorry\n"
-
+            s += " := by sorry--<Auto> \n"
         elif mode=="Lean": s+= l; s+="\n"
         elif mode=="Math": 
             tree=my_parse(l)
@@ -47,7 +31,32 @@ def toLean(text):
         else: 
             print(l)
             assert False
-    print(s)
-    return s
-            
+    
+    final = auto(s)
+    print(final)
+    return final
+
+
+def ner(l1, l2):
+    return interpret("".join([x + y for x, y in zip(l1, l2)]))[1]
+
+def auto(text):
+    l1 = list(filter((lambda x: False if x==" \n" else True), text.split("sorry--<Auto>")))
+    print(l1)
+    working=["sorry"]*len(l1)
+    found = False
+    n=-1
+    for l in l1:
+        n+=1
+        l2 = ["sorry"]*len(l1)
+        l2[n]="done"
+        nerstart = ner(l1,l2)
+        for tac in searchl:
+            l2[n]=tac
+            nerend= ner(l1,l2)
+            if nerstart > nerend:
+                working[n]=tac
+
+
+    return "".join([x + y for x, y in zip(l1, working)])
 
